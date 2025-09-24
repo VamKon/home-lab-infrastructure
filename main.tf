7392d5302446cdaec2850d644c5660b16a2e6363
VamKon <vamsee.konda@hotmail.com>
2025-09-23 21:05:37 -0500

    adding argocd

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
 # namespace
 
-resource "kubernetes_namespace" "vamsee_namespace" {
-  metadata {
-    name = "vamsee"
-    labels = {
-      mylabel = "vamsee"
-    }
-  }
+resource "helm_release" "argo-cd" {
+  name      = "argo-cd-release"
+  namespace = "argo-cd"
+  create_namespace = true
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart = "argo-cd"
+  version = "8.5.6"
+  values = [
+    file("argo-cd-values/values.yaml")
+  ]
+
+  atomic = true
 }
--- provider.tf
+++ provider.tf
@@ -2,12 +2,6 @@ provider "helm" {
   kubernetes = {
     config_path = "~/.kube/config"
   }
-
-  registries = [
-    {
-      url      = "https://argoproj.github.io/argo-helm"
-    }
-  ]
 }
 
 provider "kubernetes" {
